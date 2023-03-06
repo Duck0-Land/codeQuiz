5e9e8935457eaaf80be5db4c7f0d024d82f36dfc
Duck0-Land <erhilston@gmail.com>
2023-03-06 17:05:07 -0500

    finished ladning page

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,29 @@
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        answer: "c."
     },
     {
         title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        choices: ["a. quotes", "b. curly brackets", "c. parentheses", "d. square brackets"],
+        answer: "c."
     },
     {
         title: "Arrays in Javascript can be used to store ____.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
         answer: "all of the above"
     },
     {
         title: "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
         answer: "quotes"
     },
     {
         title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-        answer: "console log"
+        choices: ["a. Javascript", "b. terminal / bash", "c. for loops", "d. console log"],
+        answer: "d."
     },
 
 ];
 
-var currentTime = document.querySelector("#currentTime");
-var timer = document.querySelector("#startTime");
-var questionsDiv = document.querySelector("#questionsDiv");
-var wrapper = document.querySelector("#wrapper");
