2dcb5a60800878b575cffd7cdc84c54bdb465bca
Duck0-Land <erhilston@gmail.com>
2023-03-06 15:20:00 -0500

    fixing links

--- assets/script.js
+++ assets/script.js
@@ -26,3 +26,8 @@ var questions = [
     },
 
 ];
+
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
