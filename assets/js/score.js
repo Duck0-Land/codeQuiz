756ff60ccd32c8ac463e3ea72c4ad00a2d3c652a
Duck0-Land <erhilston@gmail.com>
2023-06-05 20:04:02 -0700

    added score html file

---
+++ assets/js/score.js
@@ -0,0 +1,11 @@
+// code to save and clear the scoreboard
+// variables
+let scores = document.querySelector("scores");
+let clear = document.querySelector("clear");
+
+//button to clear the scoreboard
+clear.addEventListener("click", function() {
+
+});
+
+//retrieve and load scores from local storage
